e21af1c10a4a942e4f1ce5494ef13d93e45943a5
장윤서 <snf06103@naver.com>
2026-03-30 15:16:49 +0900

    스크롤이벤트

---
+++ test/A-2/js/index.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+    //팝업기능
+    const btnPopup = document.querySelector("#btn-popup")
+    let count = 0
+
+    setInterval(()=>{
+        
+    })
+    
+})
